df9191895e7a9d8fed9441ffd667b72b5cc41198
Gorky Kirill <59337566+ToxicSnail@users.noreply.github.com>
2025-02-05 16:10:39 +0700

    Create receive_money.ts
    return money from jetton_wallet

---
+++ 2-day/scripts/receive_money.ts
@@ -0,0 +1,11 @@
+import { Address, toNano } from '@ton/core';
+import { compile, NetworkProvider } from '@ton/blueprint';
+import { jettonContentToCell, JettonMinter } from '../wrappers/JettonMinter';
+import { SaleContract } from '../wrappers/Sale';
+
+export async function run(provider: NetworkProvider) {
+    const code = await compile('Sale');
+    let contract = SaleContract.createFromConfig(code);
+    let openedContract = provider.open(contract);
+    await openedContract.sendReceiveTons(provider.sender());
+}
