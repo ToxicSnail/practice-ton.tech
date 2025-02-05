c27724c472063ce562ac6f5268934480c283cad9
Gorky Kirill <59337566+ToxicSnail@users.noreply.github.com>
2025-02-05 16:11:29 +0700

    Update setJettonWallet.ts

--- 2-day/scripts/setJettonWallet.ts
+++ 2-day/scripts/setJettonWallet.ts
@@ -1,11 +1,10 @@
 import { Address, toNano } from '@ton/core';
-import { JettonMinter, jettonContentToCell } from '../wrappers/JettonMinter';
-import { NetworkProvider, sleep , compile} from '@ton/blueprint';
+import { compile, NetworkProvider } from '@ton/blueprint';
+import { jettonContentToCell, JettonMinter } from '../wrappers/JettonMinter';
 import { SaleContract } from '../wrappers/Sale';
 
 export async function run(provider: NetworkProvider) {
-    const code = await compile('Sale');
-    let contract = SaleContract.createFromAddress(Address.parse("kQAteUAq6Llk30YNqNkBrjXcofdNHUDhfYPMrmeUKcU2Q94N"));
+    let contract = SaleContract.createFromAddress(Address.parse("kQBCHJMng-..."));
     let openedContract = provider.open(contract);
-    await openedContract.sendJettonWallet(provider.sender(), {address: Address.parse("kQBLkdEx610YzYIbQk3VWy9At8LWM-5mSne6-bG7NaygR9rz")});
+    await openedContract.sendJettonWallet(provider.sender(), { address: Address.parse("kQDNBg...")});
 }
