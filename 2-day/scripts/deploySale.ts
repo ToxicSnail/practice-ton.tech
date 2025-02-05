9a8fe1acee41c6c5bb4ce29e64ad9f5873c8681b
Gorky Kirill <59337566+ToxicSnail@users.noreply.github.com>
2025-02-05 16:03:58 +0700

    Update deploySale.ts

--- 2-day/scripts/deploySale.ts
+++ 2-day/scripts/deploySale.ts
@@ -1,10 +1,9 @@
 import { Address, toNano } from '@ton/core';
-import { JettonMinter, jettonContentToCell } from '../wrappers/JettonMinter';
-import { NetworkProvider, sleep , compile} from '@ton/blueprint';
+import { compile, NetworkProvider } from '@ton/blueprint';
+import { jettonContentToCell, JettonMinter } from '../wrappers/JettonMinter';
 import { SaleContract } from '../wrappers/Sale';
 
 export async function run(provider: NetworkProvider) {
-    let admin = Address.parse("0QC5r2uWKhsZgykpnCyJ0_B5o2fhaMDSDe9Ah8lN3qy1C9na");
     const code = await compile('Sale');
     let contract = SaleContract.createFromConfig(code);
     let openedContract = provider.open(contract);
